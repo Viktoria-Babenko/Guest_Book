c5a02ce977e9472873918ffebd1d4f84d6de3ba1
Виктория <babenko.viktoria.v@gmail.com>
2024-03-19 15:53:35 +0200

    #Task 4 Создание класса модели RegisterModel для регистрации

---
+++ Guest book/Models/RegisterModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guest_book.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string? LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пороль")]
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пороль потверждение")]
+        public string? PasswordConfirm { get; set; }
+    }
+}
