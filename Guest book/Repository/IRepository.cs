57cca280844f2fc603f77012bbc6c427d7df3237
Виктория <babenko.viktoria.v@gmail.com>
2024-03-19 16:34:19 +0200

    #Task 8 Создание интерфейса interface IRepository.  Для работы через него с классом репозитория

---
+++ Guest book/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Guest_book.Models;
+namespace Guest_book.NewFolder
+{
+    public interface IRepository
+    {
+        Task<List<MessegesModel>> GetMessegesList();
+        Task<List<UserModel>> GetUserList();
+        IQueryable<UserModel> UserWhere(string login);
+        int GetUserCount();
+        Task<UserModel> GetUser(string login);
+        Task<MessegesModel> GetMesseges(int id);
+        Task CreateUser(UserModel item);
+        Task CreateMessege(MessegesModel item, UserModel u);
+        Task Save();
+    }
+}
