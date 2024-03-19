6fe8b31038199c134765d6f71b316449e852f969
Виктория <babenko.viktoria.v@gmail.com>
2024-03-19 15:51:57 +0200

    #Task 3 Создание модели LoginModel для авторизации

---
+++ Guest book/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guest_book.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string? Password { get; set; }
+    }
+}
