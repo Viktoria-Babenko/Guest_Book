8d5645dc57cbe533d0d2ee6117745819861df331
Виктория <babenko.viktoria.v@gmail.com>
2024-03-19 16:23:03 +0200

    #Task 5 Создание класса модели  MessegesModel для хранение обьекта сообщение пользователей в базе данных.

---
+++ Guest book/Models/MessegesModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guest_book.Models
+{
+    public class MessegesModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Messeges { get; set; }
+        public DateTime? MessegeDate { get; set; }
+        public int UserId { get; set; }
+        public UserModel? User { get; set; }
+    }
+}
