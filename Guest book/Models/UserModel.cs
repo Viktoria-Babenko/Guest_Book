b13267002bb81810a40147dbe318c5ac35161d63
Виктория <babenko.viktoria.v@gmail.com>
2024-03-19 16:27:27 +0200

    #Task 6 Создание класса модели UserModel для хранения обьекта пользователя с колекцией ICollection сообщений пользователя .
    В классе данные о пользователе , поля :
    Имя , Фамилия , Логин , Пароль, Соль(для шифрования пароля) и колекция оьбектов сообщений.

---
+++ Guest book/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guest_book.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Имя")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Фамилия")]
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
+        public ICollection<MessegesModel>? Messeges { get; set; }
+        public UserModel()
+        {
+            this.Messeges = new HashSet<MessegesModel>();
+        }
+    }
