b6e788deef32d6396b07a4457dca6ee92bb7fd6e
Виктория <babenko.viktoria.v@gmail.com>
2024-04-18 12:46:02 +0300

    #Task 10 Создание UserController .
    Для регистрации и авторизации пользователей.
    Login - Action для запроса ajax - вход в аккаунт
    Register- Action для запроса ajax - регистрация пользователя.

---
+++ Guest_Book/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Guest_Book.Models;
+using Guest_Book.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Guest_Book.Controllers
+{
+    public class UserController : Controller
+    {
+        IRepository repo;
+
+        public UserController(IRepository r)
+        {
+            repo = r;
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel logon)
+        {
+            if (ModelState.IsValid)
+            {
+                if (repo.GetUserCount() == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var users = repo.UserWhere(logon.Login);
+                if (users?.ToList().Count == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var user = users?.First();
+                string? salt = user?.Salt;
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + logon.Password);
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = SHA256.HashData(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                if (user?.Password != hash.ToString())
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                string response = JsonConvert.SerializeObject(user);
+                return Json(response);
+            }
+            else
+                return Problem("Problems with authorization!");
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                UserModel user = new UserModel();
+                user.FirstName = reg.FirstName;
+                user.LastName = reg.LastName;
+                user.Login = reg.Login;
+
+                byte[] saltbuf = new byte[16];
+
+                RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+                randomNumberGenerator.GetBytes(saltbuf);
+
+                StringBuilder sb = new StringBuilder(16);
+                for (int i = 0; i < 16; i++)
+                    sb.Append(string.Format("{0:X2}", saltbuf[i]));
+                string salt = sb.ToString();
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + reg.Password);
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = SHA256.HashData(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                user.Password = hash.ToString();
+                user.Salt = salt;
+                repo.CreateUser(user);
+                repo.Save();
+                string response = user.FirstName + " " + user.LastName + " " + ", You have successfully registered!";
+                return Json(response);
+            }
+            else
+                return Problem("Problems with registration!");
+        }
+    }
+}
