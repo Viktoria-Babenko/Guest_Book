358cc2e6203327d2b54bff8d22685a8fe1e39cb0
Виктория <babenko.viktoria.v@gmail.com>
2024-04-18 12:42:06 +0300

    #Task 9 Изминения HomeController.
    Работа с контектсом базы данных через интерфес IRepository.
    GetMesseges - создание Action для ajax запроса получения коллекции сообщений.
    Позключение пакета Newtonsoft.Json для работы с  JsonConvert.Serialize

--- Guest_Book/Controllers/HomeController.cs
+++ Guest_Book/Controllers/HomeController.cs
@@ -1,23 +1,64 @@
 using Guest_Book.Models;
+using Guest_Book.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace Guest_Book.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        IRepository repo;
+        public HomeController(ILogger<HomeController> logger, IRepository r)
         {
             _logger = logger;
+            repo = r;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpGet]
+        public async Task<IActionResult> GetMesseges()
+        {
+            if (repo.GetMessegesList() == null)
+                return Problem("The message list is empty!");
+            List<MessegesModel> list = repo.GetMessegesList();
+            List<Messege> mes = new List<Messege>();
+            foreach (var item in list)
+            {
+                Messege messege = new Messege();
+                messege.Messeges = item.Messeges;
+                messege.MessegeDate = item.MessegeDate.ToString();
+                messege.User = item?.User?.FirstName;
+                mes.Add(messege);
+            }
+            string response = JsonConvert.SerializeObject(mes);
+            return Json(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> InsertMesseges(MessegesModel mess)
+        {
+            if (ModelState.IsValid)
+            {
+                if (mess.Messeges != null)
+                {
+                    MessegesModel messege = new MessegesModel();
+                    messege.MessegeDate = DateTime.Now;
+                    UserModel? user = await repo.GetUser(mess.UserId);
+                    messege.User = user;
+                    messege.UserId = user.Id;
+                    messege.Messeges = mess.Messeges;
+                    repo?.CreateMessege(messege, user);
+                    repo?.Save();
+                    string response = "Message added successfully!";
+                    return Json(response);
+                }
+                return Problem("Problems adding a message!");
+            }
+            return Problem("Problems adding a message!");
+        }
         public IActionResult Privacy()
         {
             return View();
