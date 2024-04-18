f43d20dc853806d4e38c5330afbac31f0875cf74
Виктория <babenko.viktoria.v@gmail.com>
2024-04-18 12:16:34 +0300

    #Task 1 Создание класса сущности пользователь UserModel .

---
+++ Guest_Book/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Guest_Book.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Salt { get; set; }
+        public ICollection<MessegesModel>? Messeges { get; set; }
+        public UserModel()
+        {
+            this.Messeges = new HashSet<MessegesModel>();
+        }
+    }
+}
