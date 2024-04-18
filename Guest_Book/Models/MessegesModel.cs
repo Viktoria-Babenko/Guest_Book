c043b83c10dc0eae1739e757d7da50fdf7338219
Виктория <babenko.viktoria.v@gmail.com>
2024-04-18 12:18:18 +0300

    #Task 2 Создание класса сущности сообщение MessegesModel.

---
+++ Guest_Book/Models/MessegesModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guest_Book.Models
+{
+    public class MessegesModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Messeges { get; set; }
+        public DateTime? MessegeDate { get; set; }
+        public int UserId { get; set; }
+        public UserModel? User { get; set; }
+    }
+}
