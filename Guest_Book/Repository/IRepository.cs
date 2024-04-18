d28ab9a9199262b4a0a1eb466160ba2ece5940bf
Виктория <babenko.viktoria.v@gmail.com>
2024-04-18 12:34:09 +0300

    #Task 8 Создание интерфейса IRepository и класса реализуещий интерфейс ReviewsRepository .
    Для работы с классом контекст через интерфейс.

---
+++ Guest_Book/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Guest_Book.Models;
+
+namespace Guest_Book.Repository
+{
+    public interface IRepository
+    {
+        List<MessegesModel> GetMessegesList();
+        Task<List<UserModel>> GetUserList();
+        IQueryable<UserModel> UserWhere(string login);
+        int GetUserCount();
+        Task<UserModel> GetUser(int id);
+        Task<MessegesModel> GetMesseges(int id);
+        Task CreateUser(UserModel item);
+        Task CreateMessege(MessegesModel item, UserModel u);
+        Task Save();
+    }
+}
---
+++ Guest_Book/Repository/ReviewsRepository.cs
@@ -0,0 +1,56 @@
+﻿using Guest_Book.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guest_Book.Repository
+{
+    public class ReviewsRepository : IRepository
+    {
+        private readonly ReviewsContext _context;
+
+        public ReviewsRepository(ReviewsContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<UserModel>> GetUserList()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        public int GetUserCount()
+        {
+            return _context.Users.ToList().Count;
+        }
+        public List<MessegesModel> GetMessegesList()
+        {
+            var reviewsContext = _context.Messeges.Include(p => p.User);
+            return reviewsContext.ToList();
+        }
+        public IQueryable<UserModel> UserWhere(string login)
+        {
+            return _context.Users.Where(a => a.Login == login);
+        }
+        public async Task<UserModel> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+        public async Task<MessegesModel> GetMesseges(int id)
+        {
+            return await _context.Messeges.FindAsync(id);
+        }
+
+        public async Task CreateUser(UserModel c)
+        {
+            await _context.Users.AddAsync(c);
+        }
+        public async Task CreateMessege(MessegesModel c, UserModel u)
+        {
+            await _context.Messeges.AddAsync(c);
+            await _context.SaveChangesAsync();
+            u.Messeges.Add(c);
+            _context.Entry(c).State = EntityState.Modified;
+        }
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
