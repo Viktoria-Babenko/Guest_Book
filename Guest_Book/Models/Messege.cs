28566341b8b6712028487b54f5841323394e9710
Виктория <babenko.viktoria.v@gmail.com>
2024-04-18 12:27:08 +0300

    #Task 5 Создание вспомогательной модели Messege для работы со списком сообщений.

---
+++ Guest_Book/Models/Messege.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Guest_Book.Models
+{
+    public class Messege
+    {
+        [Required]
+        public string? Messeges { get; set; }
+        public string? MessegeDate { get; set; }
+        public string? User { get; set; }
+    }
+}
